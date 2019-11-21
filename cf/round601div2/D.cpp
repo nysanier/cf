87d6d20dbed9874a433fa7ea1d05f7b35f952788
nysanier <nysanier@163.com>
2019-11-21 21:16:34 +0800

    1254A

--- cf/round601div2/D.cpp
+++ cf/round601div2/D.cpp
@@ -17,13 +17,23 @@ void Dump(const std::vector<int>& vec) {
     }
 }
 
+void Dump(char arr[][123], int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            printf("%c", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int T;
 int r, c;
 int k;
 char a[123][123];
 char res[123][123];
-int name[62];
+char name[62];
 int count[62];
+
 
 /*
 search like this(spin)
@@ -46,72 +56,87 @@ void Case() {
     scanf("%d %d %d", &r, &c, &k);
 
     // init
+    
+    for (int i = 0; i < 26; ++i) {
+        name[i] = 'A' + i;
+    }
+    for (int i = 0; i < 26; ++i) {
+        name[i + 26] = 'a' + i;
+    }
     for (int i = 0; i < 10; ++i) {
-        name[i] = '0' + i;
-    }
-    for (int i = 0; i < 26; ++i) {
-        name[i + 10] = 'A' + i;
-    }
-    for (int i = 0; i < 26; ++i) {
-        name[i + 10 + 26] = 'a' + i;
-    }
-
-    std::fill(count, count + k, 0);
-    int n = r * c;
-    while (n > 0) {
-        for (int i = 0; i < k; ++i) {
-            count[i] += 1;
-            n -= 1;
-            if (n == 0) {
-                break;
-            }
-        }
+        name[i + 52] = '0' + i;
     }
 
     // x is border
     std::fill(a[0], a[0] + c + 2, 'x');
+    std::fill(res[0], res[0] + c + 2, 'x');
     for (int i = 1; i <= r; ++i) {
         a[i][0] = 'x';
-        scanf("%s", &a[i][1]);
+        scanf("%s", a[i] + 1);
         a[i][c+1] = 'x';
+        std::fill(res[i], res[i] + c + 2, 'x');
     }
     std::fill(a[r+1], a[r+1] + c + 2, 'x');
+    std::fill(res[r+1], res[r+1] + c + 2, 'x');
+    // Dump(a, r + 2, c + 2);
+
+    std::fill(count, count + k, 0);
+    int n = 0;  // count of R
+    for (int i = 1; i <= r; ++i) {
+        for (int j = 1; j <= c; ++j) {
+            if (a[i][j] == 'R') {
+                n += 1;
+            }
+        }
+    }
+    while (n > 0) {
+        for (int i = 0; i < k && n > 0; ++i) {
+            count[i] += 1;
+            n -= 1;
+        }
+    }
 
     int x = 1;
     int y = 1;
     int d = 0;
     int idx = 0;
     int dir_change_count = 0;
-    int rice_count = 1;
+    int rice_count = a[x][y] == 'R' ? 1 : 0;
     a[x][y] = 'x';
     res[x][y] = name[idx];
     for (;;) {
+        if (dir_change_count > 1) {
+            break;
+        }
+
         int x2 = x + dir[d][0];
         int y2 = y + dir[d][1];
         if (a[x2][y2] == 'x') {
-            d = (d + 1) % 4;  // next direction
+            d = (d + 1) % 4;  // next direction, clockwise
             dir_change_count += 1;
-            if (dir_change_count > 1) {
-                break;  // final
-            } else {
-                continue;
-            }
-        }
-
-        if (a[x2][y2] == '.') {
-            a[x2][y2] = 'x';
-            res[x][y] = name[idx];
             continue;
         }
 
-        //  a[x2][y2] == 'R'
-        if (rice_count > count[idx]) {
-            rice_count = 0;
-            idx += 1;
+        dir_change_count = 0;
+        if (a[x2][y2] == 'R') {
+            if (rice_count + 1 > count[idx]) {
+                rice_count = 1;
+                idx += 1;
+            } else {
+                rice_count += 1;
+            }
+            a[x2][y2] = 'x';
+            res[x2][y2] = name[idx];
+            x = x2;
+            y = y2;
+            continue;
         }
+
+        assert(a[x2][y2] == '.');
         a[x2][y2] = 'x';
-        res[x][y] = name[idx];
-        rice_count += 1;
+        res[x2][y2] = name[idx];
+        x = x2;
+        y = y2;
     }
 
     for (int i = 1; i <= r; ++i) {
@@ -123,7 +148,7 @@ void Case() {
 }
 
 int main() {
-    // printf(">\n");
+    //  printf(">\n");
     scanf("%d", &T);
     while (T--) {
         Case();
