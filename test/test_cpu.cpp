dc331ee69ca25d85e261aad6e989eb56cd5aed3e
nysanier <nysanier@163.com>
2019-11-29 11:06:43 +0800

    fix

--- test/test_cpu.cpp
+++ test/test_cpu.cpp
@@ -1,23 +1,32 @@
 #include "bits/stdc++.h"
 
-#define BENCH_TIMER
+// #define BENCH_TIMER
 
 #ifdef BENCH_TIMER
 # include <boost/timer/timer.hpp>
 #endif
 
-const int N = 1e8 + 9;
-// NlogN, N = 1e6:
+const int N = 1e7 + 9;
+// O(NlogN), N = 1e6:
 //   i5-7600: 0.063968s wall, 0.060000s user + 0.000000s system = 0.060000s CPU (93.8%)
 //   oj: 93 ms	3900 KB
 
-// NlogN, N = 1e7:
+// O(NlogN), N = 1e7:
 //   i5-7600: 0.751747s wall, 0.740000s user + 0.000000s system = 0.740000s CPU (98.4%)
 //   oj: 701 ms	39100 KB
 
-// N^2(1e4) or N(1e8):
+// O(N^2), 1e4:
 //   i5-7600: 0.055466s wall, 0.050000s user + 0.000000s system = 0.050000s CPU (90.1%)
 //   oj: 156 ms	0 KB
+
+// O(N), 1e8:
+//   i7-8700: 0.591783s wall, 0.500000s user + 0.080000s system = 0.580000s CPU (98.0%)
+//   oj: 280 ms	262100 KB(Memory out)
+
+// O(N), 1e7:
+//   i7-8700: 0.061349s wall, 0.050000s user + 0.010000s system = 0.060000s CPU (97.8%)
+//   oj: 109 ms	39100 KB, 单核性能弱了一倍左右?
+
 
 int a[N];
 
