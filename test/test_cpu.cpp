77eeaee4078357be985706e6805903e558873680
nysanier <nysanier@163.com>
2019-11-29 10:56:40 +0800

    fix test cpu

--- test/test_cpu.cpp
+++ test/test_cpu.cpp
@@ -1,34 +1,4 @@
-// C
-#include <stdio.h>
-#include <stdint.h>
-#include <string.h>
-#include <assert.h>
-#include <math.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-
-// C++
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <iomanip>
-#include <iostream>
-#include <istream>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <new>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <string>
-#include <utility>
-#include <vector>
+#include "bits/stdc++.h"
 
 #define BENCH_TIMER
 
@@ -36,7 +6,7 @@
 # include <boost/timer/timer.hpp>
 #endif
 
-const int N = 1e4 + 9;
+const int N = 1e8 + 9;
 // NlogN, N = 1e6:
 //   i5-7600: 0.063968s wall, 0.060000s user + 0.000000s system = 0.060000s CPU (93.8%)
 //   oj: 93 ms	3900 KB
@@ -45,7 +15,7 @@ const int N = 1e4 + 9;
 //   i5-7600: 0.751747s wall, 0.740000s user + 0.000000s system = 0.740000s CPU (98.4%)
 //   oj: 701 ms	39100 KB
 
-// N^2, N = 1e4:
+// N^2(1e4) or N(1e8):
 //   i5-7600: 0.055466s wall, 0.050000s user + 0.000000s system = 0.050000s CPU (90.1%)
 //   oj: 156 ms	0 KB
 
@@ -67,12 +37,14 @@ void TestN() {
 
 void TestLogN() {
     AUTO_TIMER;
+    srand(0);
     TestN();
     std::sort(a, a + N);
 }
 
 void TestN2() {
     AUTO_TIMER;
+    srand(0);
     TestN();
     for (int i = 0; i < N; ++i) {
         for (int j = i + 1; j < N; ++j) {
@@ -84,8 +56,9 @@ void TestN2() {
 }
 
 int main() {
+    TestN();
     // TestLogN();
-    TestN2();
+    // TestN2();
 
     return 0;
 }
