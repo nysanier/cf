2049c122cf9c06896be910469a3a4b3acb914de2
nysanier <nysanier@163.com>
2019-12-06 03:18:21 +0800

    fix 1265A

--- cf/contest/cf604/1265A.cpp
+++ cf/contest/cf604/1265A.cpp
@@ -20,30 +20,31 @@ using Sll = std::set<ll>;
 const int N = 1e5 + 9;
 char s[N];
 
-char Get(char x, char y) {
-    if (x == y) {
-        if (x == 'c') {
-            return 'a';
-        } else {
-            return x + 1;
-        }
-    }
-
-    if (x > y) {
-        std::swap(x, y);
-    }
-
-    if (x == 'a' && y == 'b') {
-        return 'c';
-    }
-
-    if (x == 'a' && y == 'c') {
-        return 'b';
-    }
-
-    if (x == 'b' && y == 'c') {
+char Next(char c) {
+    if (c == 'c')
         return 'a';
+    return c + 1;
+}
+
+char Get(char x, char y) {
+    if (x == '?' || y == '?') {
+        if (x == '?' && y == '?')
+            return 'a';
+        if (y == '?')
+            std::swap(x, y);
+        return Next(y);
     }
+
+    if (x == y)
+        return Next(x);
+    if (x > y)
+        std::swap(x, y);
+    if (x == 'a' && y == 'b')
+        return 'c';
+    if (x == 'a' && y == 'c')
+        return 'b';
+    if (x == 'b' && y == 'c')
+        return 'a';
 
     assert(0);
     return 'X';
@@ -52,48 +53,28 @@ char Get(char x, char y) {
 void Solve() {
     scanf("%s", s);
     std::string str(s);
-    while (true) {
-        auto p1 = str.find_first_of('?');
-        if (p1 == str.npos) {
-            break;
+    int n = str.size();
+    if (n == 1u) {
+        if (str[0] == '?') {
+            str[0] = Get('?', '?');
         }
-        char x;
-        if (p1 == 0u) {
-            x = 'a';  // subian
-        } else {
-            x = str[p1-1];
-        }
-        int y;
-        auto p2 = str.find_first_not_of('?', p1);
-        if (p2 == str.npos) {
-            p2 = str.size();
-            y = 'b';  // suibian
-        } else {
-            y = str[p2];
-        }
-
-        std::string re;
-        int len = p2 - p1;
-        if (len % 2 == 0) {
-            // puts("% 2 == 0");
-            re.resize(2);
-            re[1] = Get(x, y);
-            re[0] = Get(x, re[1]);
-        } else {
-            // puts("% 2 == 1");
-            re.resize(2);
-            re[0] = Get(x, y);
-            re[1] = Get(x, re[0]);
-        }
-        // printf("re = '%s'\n", re.c_str());
-
-        int j = 0;
-        for (auto i = p1; i < p2; ++i, j = (j + 1) % 2) {
-            str[i] = re[j];
-        }
+        printf("%s\n", str.c_str());
+        return;
     }
 
-    // printf("%s\n", str.c_str());
+    if (str[0] == '?') {
+        str[0] = Get('?', str[1]);
+    }
+
+    if (str[n-1] == '?') {
+        str[n-1] = Get(str[n-2], '?');
+    }
+
+    forxy(i, 1, n-2) {
+        if (str[i] == '?')
+            str[i] = Get(str[i-1], str[i+1]);
+    }
+
     auto it = std::unique(str.begin(), str.end());
     if (it != str.end()) {
         puts("-1");
