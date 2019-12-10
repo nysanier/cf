0528bb89b00bcb4909a37a7f084fe1b8fb297196
nysanier <nysanier@163.com>
2019-12-10 15:28:26 +0800

    fix dump.h

--- include/dump.h
+++ include/dump.h
@@ -5,23 +5,25 @@
 // debug
 template<typename... Args>
 std::string format(const std::string& fmt, Args... args) {
-    auto sz = ::snprintf(nullptr, 0, fmt.c_str(), args...);
+    auto sz = ::snprintf(nullptr, 0, fmt.c_str(), args...);  // effective char size
     std::string buf(sz, '?');
-    ::snprintf(&buf[0], sz + 1, fmt.c_str(), args...);
+    ::snprintf(&buf[0], sz + 1, fmt.c_str(), args...);  // len include '\0'
     return buf;
 }
 #ifndef ONLINE_JUDGE
-# define dbg(fmt, args...) (std::clog << "***DEBUG*** " << format(fmt, ##args) << std::endl)
+# define PRE() (std::clog << "***DEBUG*** ")
+# define POST() std::endl
+# define dbg(fmt, args...) (PRE() << format(fmt, ##args) << POST())
 # define VAR(e) (#e "=") << (e) << (",")
 # define VAR2(e1,e2) VAR(e1) << VAR(e2)
 # define VAR4(e1,e2,e3,e4) VAR2(e1,e2) << VAR2(e3,e4)
-# define dump(e) (std::clog << "***DEBUG*** " << VAR(e) << std::endl)
-# define dump2(e1,e2) (std::clog << "***DEBUG*** " << VAR2(e1,e2) << std::endl)
-# define dump3(e1,e2,e3) (std::clog << "***DEBUG*** " << VAR2(e1,e2) << VAR(e3) << std::endl)
-# define dump4(e1,e2,e3,e4) (std::clog << "***DEBUG*** " << VAR4(e1,e2,e3,e4) << std::endl)
-# define dump5(e1,e2,e3,e4,e5) (std::clog << "***DEBUG*** " << VAR4(e1,e2,e3,e4) << VAR(e5) << std::endl)
-# define dump6(e1,e2,e3,e4,e5,e6) (std::clog << "***DEBUG*** " << VAR4(e1,e2,e3,e4) << VAR2(e5,e6) << std::endl)
-# define dump7(e1,e2,e3,e4,e5,e6,e7) (std::clog << "***DEBUG*** " << VAR4(e1,e2,e3,e4) << VAR2(e5,e6) << VAR(e7) << std::endl)
+# define dump(e) (PRE() << VAR(e) << POST())
+# define dump2(e1,e2) (PRE() << VAR2(e1,e2) << POST())
+# define dump3(e1,e2,e3) (PRE() << VAR2(e1,e2) << VAR(e3) << POST())
+# define dump4(e1,e2,e3,e4) (PRE() << VAR4(e1,e2,e3,e4) << POST())
+# define dump5(e1,e2,e3,e4,e5) (PRE() << VAR4(e1,e2,e3,e4) << VAR(e5) << POST())
+# define dump6(e1,e2,e3,e4,e5,e6) (PRE() << VAR4(e1,e2,e3,e4) << VAR2(e5,e6) << POST())
+# define dump7(e1,e2,e3,e4,e5,e6,e7) (PRE() << VAR4(e1,e2,e3,e4) << VAR2(e5,e6) << VAR(e7) << POST())
 #else
 # define dbg(fmt, args...)
 # define dump(e)
