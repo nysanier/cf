b568c7ecd7785e3ea5f211b319091ba1f447f8a3
nysanier <nysanier@163.com>
2019-12-10 11:17:23 +0800

    support windows

---
+++ build.bat
@@ -0,0 +1,5 @@
+REM "C:\Program Files (x86)\mingw-w64\i686-6.4.0-posix-dwarf-rt_v5-rev0\mingw-w64.bat"
+mkdir cbuild_win
+cd cbuild_win
+cmake .. -G "MinGW Makefiles"
+REM "C:\Program Files (x86)\mingw-w64\i686-6.4.0-posix-dwarf-rt_v5-rev0\mingw32\bin\mingw32-make"
