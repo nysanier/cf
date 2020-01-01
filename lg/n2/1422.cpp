601ef72baa952b6f3ba47147b97a718caca8d88a
nysanier <nysanier@163.com>
2020-01-01 10:34:21 +0800

    add lg/n2

---
+++ lg/n2/1422.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+// #include <bits/extc++.h>
+#ifndef ONLINE_JUDGE
+# include "dump.h"
+#else
+# define DUMP(args...)
+# define LINE(e)
+#endif
+
+// abbr
+#define forn(i, x, n) for (long i = x; i < n+x; ++i)
+#define fornr(i, x, n) for (long i = n-1+x; i >= x; --i)  // must be signed, or 0->~0
+#define for0(i, n) forn(i, 0, n)
+#define for1(i, n) forn(i, 1, n)
+#define for0r(i, n) fornr(i, 0, n)
+#define for1r(i, n) fornr(i, 1, n)
+#define forxyd(i, x, y, d) for (long i = x; i <= y; i += d)
+#define foryxd(i, x, y, d) for (long i = y; i >= x; i -= d)
+#define forxy(i, x, y) forxyd(i, x, y, 1)
+#define foryx(i, x, y) foryxd(i, x, y, 1)
+#define col(e) (std::cout << (e) << std::endl)
+#define co(e) (std::cout << (e) << " ")
+
+// type
+using ll = int32_t;
+// using ll = int64_t;
+using Vec = std::vector<ll>;
+using Deq = std::deque<ll>;
+using Lst = std::list<ll>;
+using Pr = std::pair<ll, ll>;
+using St = std::set<ll>;
+using Mst = std::multiset<ll>;
+using Ust = std::unordered_set<ll>;
+using Mp = std::map<ll, ll>;
+using Mmp = std::multimap<ll, ll>;
+using Ump = std::unordered_map<ll, ll>;
+
+// -------------------------------------------------
+int n;
+void Init() {}
+void Solve() {
+    std::cin >> n;
+    double sum = 0;
+    auto a = std::min(150, n);
+    auto b = std::min(n - 150, 400-150);
+    if (b < 0) b = 0;
+    auto c = n - 400;
+    if (c < 0) c = 0;
+    sum = a*0.4463 + b*0.4663 + c*0.5663;
+    printf("%.1lf\n", sum);
+}
+// -------------------------------------------------
+
+int main() {
+#ifndef ONLINE_JUDGE
+    ::freopen("../input.txt", "r", stdin);
+#endif
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    Init();
+    ll t = 1;
+    // std::cin >> t;
+    for0(i, t) Solve();
+
+    return 0;
+}
