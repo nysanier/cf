5fe2a14e155cae2d2c1e59a1f60c4718fc3b1e2d
nysanier <nysanier@163.com>
2020-01-04 10:59:19 +0800

    fix 1308 find word

--- lg/newbie/n5/1308.cpp
+++ lg/newbie/n5/1308.cpp
@@ -41,7 +41,7 @@ void ToLower(std::string& str) {
     for (auto& c : str)
         c = std::tolower(c);
 }
-void Solve2() {
+void Solve() {
     std::string word;
     std::getline(std::cin, word);
     ToLower(word);
@@ -58,43 +58,59 @@ void Solve2() {
         col(-1);
         return;
     }
-    auto p = text.find(word);
-    co(cnt); col(p);
-}
-void Solve() {
-    using namespace std;
-    //定义两个字符串
-    string a;
-    string b;
-    //用string库，调用getline, 直接读入一整行
-    getline(cin,a);
-    getline(cin,b);
-    //转换大小写，可以都转换为大写，或者小写
-    for (int i=0;i<a.length();++i){
-        a[i]=tolower(a[i]);
-    }
-    for (int i=0;i<b.length();++i){
-        b[i]=tolower(b[i]);
-    }
-    //因为连起来的不算，所以要在前后加几个空格，一定要是同样多的，同量减同量，等于同量
-    a=' '+a+' ';
-    b=' '+b+' ';
-    //先看看会不会找不到，用a.find()和string::npos
-    if (b.find(a)==string::npos){
-        cout<<-1<<endl;
-    }
-    //如果找得到
-    else {
-        int alpha=b.find(a);
-        int beta=b.find(a),s=0;//计数器初始化为0
-        while (beta!=string::npos){
-            ++s;//计数器
-            beta=b.find(a,beta+1);
+    // 问题在这里: 会搜索到其他单词的内部!!!
+    // 所以只要判断搜索到的这个位置前后都是空格不就好了吗?
+    std::string::size_type p1 = 0, p2;
+    for (;;) {
+        p2 = text.find(word, p1);
+        assert(p2 != text.npos);
+        auto p3 = p2 + word.size();
+        assert(p3 <= text.size());
+        if ((p2==0 || text[p2-1] == ' ') && (p3 == text.size() || text[p3] == ' ')) {
+            break;
         }
-        cout<<s<<" "<<alpha<<endl;//输出第一个和总共有几个
+
+        // 还可以去找下一个 ' ' 来进一步提升效率
+        // p2 = text.find_first_of(' ', p3);
+        p1 = p3;
+        // p1 = p1 + 1;  // 效率太低, 1e6上面直接失败
     }
-    //函数返回值为0，结束整个程序
+    co(cnt); col(p2);
 }
+// void Solve2() {
+//     using namespace std;
+//     //定义两个字符串
+//     string a;
+//     string b;
+//     //用string库，调用getline, 直接读入一整行
+//     getline(cin,a);
+//     getline(cin,b);
+//     //转换大小写，可以都转换为大写，或者小写
+//     for (int i=0;i<a.length();++i){
+//         a[i]=tolower(a[i]);
+//     }
+//     for (int i=0;i<b.length();++i){
+//         b[i]=tolower(b[i]);
+//     }
+//     //因为连起来的不算，所以要在前后加几个空格，一定要是同样多的，同量减同量，等于同量
+//     a=' '+a+' ';
+//     b=' '+b+' ';
+//     //先看看会不会找不到，用a.find()和string::npos
+//     if (b.find(a)==string::npos){
+//         cout<<-1<<endl;
+//     }
+//     //如果找得到
+//     else {
+//         int alpha=b.find(a);
+//         int beta=b.find(a),s=0;//计数器初始化为0
+//         while (beta!=string::npos){
+//             ++s;//计数器
+//             beta=b.find(a,beta+1);
+//         }
+//         cout<<s<<" "<<alpha<<endl;//输出第一个和总共有几个
+//     }
+//     //函数返回值为0，结束整个程序
+// }
 // -------------------------------------------------
 
 int main() {
