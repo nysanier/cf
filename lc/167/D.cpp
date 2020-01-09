b4a8370f888f3d45c424ef3c6da4d1a4838266dc
nysanier <nysanier@163.com>
2020-01-09 15:07:26 +0800

    add lc/167/D

--- lc/167/D.cpp
+++ lc/167/D.cpp
@@ -8,63 +8,45 @@ using namespace std;
 
 // # define DUMP(args...)
 const int N = 40 + 9;
-const int INF = 1e9;
-int a[N*N][N][N];
+int vis[N][N];
+const int dir[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 class Solution {
+    struct S {
+        int r, c;  // 当前的位置
+        int k;  // 当前还能经过的障碍物个数
+        int d;  // 当前经过的步数
+    };
     int m, n;
-    vector<vector<int>> g;
     void init() {
-        // memset(a, -1, sizeof(a));
-        for (int i = 0; i < N; ++i)
-            for (int j = 0; j < N; ++j)
-                std::fill(a[i][j], a[i][j]+N*N, INF);
+        memset(vis, 0, sizeof(vis));
     }
-    int dir[4][2] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 public:
     // 递推式写不出来啊！！！！
+    // 这类题居然用的是bfs！！！怪不得写不错递推式，而且C已经考察了递推，那么D就不应该是递推
     int shortestPath(vector<vector<int>>& grid, int k) {
         init();
         m = grid.size();
         n = grid[0].size();
-        // a[0][0][0] = 0;
-        for (int p = 0; p <= k; ++p) {
-            for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    // init
-                    if (i == 0 && j == 0) { a[p][i][j] = 0; continue; }
-                    if (p > 0) a[p][i][j] = std::min(a[p][i][j], a[p-1][i][j]);
-                    else a[p][i][j] = INF;
-                    // iterator
-                    for (int d = 0; d < 4; ++d) {
-                        auto i2 = i + dir[d][0];
-                        auto j2 = j + dir[d][1];
-                        if (i2<0 || i2>=m || j2<0 || j2>=n) continue;
-                        // if (p == 0) {  // 初次计算
-                        //     if (grid[i][j] == 0)  // 只计算可达位置
-                        //         a[p][i][j] = std::min(a[p][i][j], a[p][i2][j2]+1);
-                        //     continue;
-                        // }
-                        if (grid[i][j] == 0) {
-                            a[p][i][j] = std::min(a[p][i][j], a[p][i2][j2]+1);
-                            if (grid[i2][j2] == 0)
-                                a[p][i2][j2] = std::min(a[p][i2][j2], a[p][i][j]+1);
-                        } else if (p > 0) {
-                            a[p][i][j] = std::min(a[p][i][j], a[p-1][i2][j2]+1);
-                            a[p][i2][j2] = std::min(a[p][i2][j2], a[p][i][j]+1);
-                        }
-                    }
+        std::queue<S> q;
+        q.push({0,0,k,0});
+        vis[0][0] = 1;
+        while (!q.empty()) {
+            auto t = q.front(); q.pop();
+            if (t.r == m-1 && t.c == n-1) return t.d;
+            for (int d = 0; d < 4; ++d) {
+                auto nr = t.r + dir[d][0];
+                auto nc = t.c + dir[d][1];
+                if (nr<0 || nr>=m || nc<0 || nc>=n) continue;
+                auto nk = t.k - (grid[nr][nc]?1:0);
+                auto nd = t.d + 1;
+                if (nk < 0) continue;
+                if (!vis[nr][nc]) {
+                    vis[nr][nc] = 1;
+                    q.push({nr,nc,nk,nd});
                 }
             }
-
-            for (int i = 0; i < m; ++i) {
-                std::vector<int> vec(a[p][i], a[p][i]+n);
-                DUMP(p, vec);
-            }
         }
-
-        auto r = a[m-1][n-1][k];
-        if (r == INF) return -1;
-        else return r;
+        return -1;  // 找不到解决方案
     }
 };
 
