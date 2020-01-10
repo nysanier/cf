c65c2013a7fe2bf9011be8bd64022acb04147490
nysanier <nysanier@163.com>
2020-01-10 23:47:11 +0800

    add lg/basic/n2/D

--- lg/basic/n2/lg_D.cpp
+++ lg/basic/n2/lg_D.cpp
@@ -36,15 +36,55 @@ using Mmp = std::multimap<ll, ll>;
 using Ump = std::unordered_map<ll, ll>;
 
 // -------------------------------------------------
-const ll N = 2e5 + 9;
-ll n;
-ll a[N];
+const ll N = 20 + 9;
+struct S {
+    ll r, c;
+    ll v;
+    bool operator<(const S& rhs) const {
+        return v > rhs.v;
+    }
+};
+ll m, n, k;
+ll a[N][N];
+Pr b[N*N];  // 摘到的个数，以及摘完这个回到路边的后k的余值
+// 找到最有一个k大于0的只就行，或者反着找到第一个k>=0的只就好
 void Init() {}
 void Solve() {
-    std::cin >> n;
-    for0(i, n) {
-        std::cin >> a[i];
+    std::cin >> m >> n >> k;
+    std::vector<S> vec;
+    for1(i, m) for1(j, n) {
+        std::cin >> a[i][j];
+        if (a[i][j] > 0)
+            vec.push_back({i,j,a[i][j]});
     }
+    if (vec.empty()) {
+        col(0);
+        return;
+    }
+    std::sort(vec.begin(), vec.end());
+    DUMP(m, n, k);
+    ll lr = 0;
+    ll lc = vec[0].c;
+    ll lv = 0;
+    // DUMP(lr, lc, k);
+    for (ll i = 0; i < vec.size(); ++i) {
+        auto d = abs(vec[i].r - lr) + abs(vec[i].c - lc);
+        b[i].first = vec[i].v + lv;
+        b[i].second = k - d - 1 - vec[i].r;  // 去/摘/回
+        k -= d + 1;
+        lr = vec[i].r;
+        lc = vec[i].c;
+        lv += vec[i].v;
+        // DUMP(i, d, lr, lc, k, ans);
+    }
+    ll ans = 0;  // 可能一个都摘不到，那么默认是0
+    for0r(i, vec.size()) {
+        if (b[i].second >= 0) {
+            ans = b[i].first;
+            break;
+        }
+    }
+    col(ans);
 }
 // -------------------------------------------------
 
