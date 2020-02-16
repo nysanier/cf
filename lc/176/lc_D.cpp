b358f1ab3be6fae59e21b8b0d7e78a3dede848a0
nysanier <nysanier@163.com>
2020-02-16 09:55:11 +0800

    fix lc/templates

---
+++ lc/176/lc_D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+// #include <bits/extc++.h>
+#include "lc.h"
+
+// -------------------------------------------------
+namespace {
+using namespace std;
+
+// # define DUMP(args...)
+class Solution {
+public:
+    bool func(int arg0, int arg1, int arg2, int arg3) {
+        return true;
+    }
+};
+
+}
+
+void Init() {}
+void Solve() {
+    using namespace std;
+
+    static int idx = 0;
+    vector<string> in;
+    std::string out;
+    lc::Read(in, out);
+    DUMP("------", idx, in, out);
+    idx +=1;
+
+    #define FUNC func
+    #define ARGS arg0, arg1, arg2, arg3
+    int arg0;
+    int arg1;
+    int arg2;
+    int arg3;
+    lc::ParseArg(in[0], arg0);
+    lc::ParseArg(in[1], arg1);
+    lc::ParseArg(in[2], arg2);
+    lc::ParseArg(in[3], arg3);
+    DUMP(ARGS);
+
+    Solution sol;
+    auto r = sol.FUNC(ARGS);
+    DUMP(out, r);
+    // assert(out == to_string(r));
+}
+// -------------------------------------------------
+
+int main() {
+#ifndef ONLINE_JUDGE
+    ::freopen("../input.txt", "r", stdin);
+#endif
+
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    Init();
+    int t = 1;
+    std::cin >> t;
+    while (t--) Solve();
+
+    return 0;
+}
