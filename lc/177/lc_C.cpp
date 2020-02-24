7103d43b1c3467fd7363e74e7bc237960144c67b
nysanier <nysanier@163.com>
2020-02-24 13:38:13 +0800

    add lc/177/C

--- lc/177/lc_C.cpp
+++ lc/177/lc_C.cpp
@@ -2,149 +2,38 @@
 // #include <bits/extc++.h>
 #include "lc.h"
 
-// 1 分解质因素
-// 2 bruteforce 找到最接近的两个因素
-// 3 x 和 y 比较
+// 找最大因子, <=2^16
+// 解题思路 均值不等式可知 a+b>=2*sqrt(a*b)
+//   两个数和固定的情况下，两数越接近，他们的乘积越大。
+//   所以，直接先开根号，然后递减遍历，当数字可以被sum整除，就可以直接返回了，不需要继续遍历，这个时候得到的乘积，一定是最大的。
+//   最后只要比较num+1得到的因子和num+2得到的因子，差的绝对值较小的那个即可
 // -------------------------------------------------
 namespace {
 using namespace std;
 
 // # define DUMP(args...)
 class Solution {
-    void f1(int n, map<int, int>& mp) {
-        mp.clear();
-        while (n > 1) {
-            bool flag = false;  // 是否找到质因数, 如果找不到, 那么本身就已经是质数了
-            for (int i = 2; i * i <= n; ++i) {
-                if (n % i == 0) {
-                    DUMP(n, i);
-                    mp[i] += 1;
-                    n /= i;
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                mp[n] += 1;
-                n = 1;
-            }
-            // DUMP("next", n);
+    // 找最大因子, <=2^16
+    vector<int> getFac(int n) {
+        int sq = ceil(sqrt(n));
+        for (int i = sq; i > 0; --i) {
+            if (n % i == 0)
+                return {i, n/i};
         }
+        // 不可能, 每个数都可以被1除
+        assert(0);
+        return {0,0};
     }
-
-    int cm(int n, int m, vector<int> out, vector<vector<int>>& total) {
-        if (m==0)
-            return 1;
-        int x = n;
-        while (x >= m) {
-            out.push_back(x);
-            if (cm(x-1, m-1, out, total)) {
-                // DUMP(out);
-                total.push_back(out);
-            }
-            out.pop_back();
-            x -= 1;
-        }
-        return 0;
-    }
-
-    // 组合
-    void fc(map<int, int>& mp, int& a, int& b) {
-        vector<int> vec;
-        int total = 1;
-        for (auto& kv : mp)
-            if (kv.second > 0) {
-                for (int i = 0; i < kv.second; ++i)
-                    vec.push_back(kv.first);
-                total *= kv.first * kv.second;
-            }
-        DUMP(vec);
-        if (vec.empty()) {
-            a = b = 1;
-            return;
-        } else if (vec.size() == 1) {
-            a = 1;
-            b = vec[0];
-            return;
-        } else if (vec.size() == 2) {
-            a = vec[0];
-            b = vec[1];
-            return;
-        }
-
-        // 暴力求解
-        int d = 1e8;
-        vector<vector<int>> vvec;
-        for (int i = 1; i < (vec.size()+2) / 2; ++i) {
-            vector<int> out;
-            cm(vec.size(), i, out, vvec);
-        }
-        DUMP(vvec);
-        for (auto v1 : vvec) {
-            int a1 = 1;
-            for (auto v2 : v1)
-                a1 *= vec[v2-1];
-            int b1 = total / a1;
-            int d1 = abs(a1 - b1);
-            // DUMP(d1, a1, b1);
-            if (d1 < d) {
-                d = d1;
-                a = a1;
-                b = b1;
-                // DUMP(d, a, b);
-            }
-        }
-    }
-
-    void f2(map<int, int>& mp, int& d, vector<int>& r) {
-        int c = 1;  // 公约数
-        // for (auto& kv : mp) {
-        //     if (kv.second > 1) {
-        //         int x = kv.second / 2;
-        //         c *= kv.first * x;
-        //         kv.second -= x * 2;
-        //     }
-        // }
-        DUMP(c);
-
-        int a, b;
-        fc(mp, a, b);
-        a *= c;
-        b *= c;
-        r.push_back(a);
-        r.push_back(b);
-        d = abs(a-b);
-    }
-
 public:
     vector<int> closestDivisors(int num) {
-        // vector<int> cmout;
-        // auto cmr = cm(5, 3, cmout);
-        // DUMP(5, 3, cmout, cmr);
-
-
-        map<int, int> mp1, mp2;
-        int n1 = num + 1;
-        int n2 = num + 2;
-        f1(n1, mp1);
-        f1(n2, mp2);
-        DUMP(n1, mp1);
-        DUMP(n2, mp2);
-
-        int d1, d2;
-        vector<int> r1, r2;
-        vector<int> r;
-        f2(mp1, d1, r1);
-        f2(mp2, d2, r2);
-        // DUMP(d1, r1);
-        // DUMP(d2, r2);
-        if (d1 < d2) {
-            r.swap(r1);
-        } else {
-            r.swap(r2);
-        }
-    
-        return r;
+        auto x = getFac(num+1);
+        auto y = getFac(num+2);
+        std::sort(x.begin(), x.end());
+        std::sort(y.begin(), y.end());
+        if (x[1]-x[0] < y[1]-y[0])
+            return x;
+        else
+            return y;
     }
 };
 
